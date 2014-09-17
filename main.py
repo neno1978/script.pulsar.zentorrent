0b5239f1e45aa2142bd6ce698f4997cec66ba358
neno1978 <titinoytitina.marcob@me.com>
2014-09-17 12:37:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,19 @@ def search(query):
     if response.headers.get("Content-Encoding", "") == "gzip":
         import zlib
         data = zlib.decompressobj(16 + zlib.MAX_WBITS).decompress(data)
+        
+    ptorrent = re.compile(r'http[s]?://.*\.torrent')
+  sections = (data.split("<td"))
+  uris = []
+  
+  for section in sections:
+      torrent = ptorrent.search(section)
+      if torrent != None:
+        uris.append({"uri": torrent2magnet(torrent.group(0))})
+    else:
+      uris.append({"uri": magnet.group(0)})
+  
+  return uris
 
     #convert the rss feed to an xml dom and get all the "<item>" elements that has a category of "cine"
     items = minidom.parseString(data).getElementsByTagName("item")
@@ -35,6 +48,16 @@ def search_episode(imdb_id, tvdb_id, name, season, episode):
 
 def search_movie(imdb_id, name, year):
     return search(imdb_id)
+
+def torrent2magnet(torrent_url):
+  response = urllib2.urlopen(torrent_url)
+  torrent = response.read()
+  metadata = bencode.bdecode(torrent)
+  hashcontents = bencode.bencode(metadata['info'])
+  digest = hashlib.sha1(hashcontents).digest()
+  b32hash = base64.b32encode(digest)
+  magneturl = 'magnet:?xt=urn:btih:' + b32hash  + '&dn=' + metadata['info']['name']
+  return magneturl    
 
 urllib2.urlopen(
     PAYLOAD["callback_url"],
